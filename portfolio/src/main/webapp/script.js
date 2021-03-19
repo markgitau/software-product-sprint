853d240155b0c9c6bbd75cdedae4ff9a3e6352e0
markgitau <mark.gitau.9@gmail.com>
2021-03-19 02:17:47 +0000

    Basic maps functionality

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -1,49 +1,31 @@
-// Copyright 2020 Google LLC
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     https://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
-/**
- * Adds a random greeting to the page.
- */
-// function addRandomGreeting() {
-//   const greetings =
-//       ['Hello world!', '¡Hola Mundo!', '你好，世界！', 'Bonjour le monde!'];
-
-//   // Pick a random greeting.
-//   const greeting = greetings[Math.floor(Math.random() * greetings.length)];
-
-//   // Add it to the page.
-//   const greetingContainer = document.getElementById('greeting-container');
-//   greetingContainer.innerText = greeting;
-// }
-
 async function showMessage() {
   const responseFromServer = await fetch('/hello');
   const msgResponse = await responseFromServer.json();
 
   const msgContainer = document.getElementById('message');
-  msgContainer.innerHTML = '';
-  msgContainer.appendChild(
-      createListElement(msgResponse.msg1));
-  msgContainer.appendChild(
-      createListElement(msgResponse.msg3));
-  msgContainer.appendChild(
-      createListElement(msgResponse.msg3));      
+  const num = Math.floor(Math.random() * 3);
+  text = "";
+  if (num == 0){
+    text = msgResponse.msg1;
+  }
+  else if (num == 1){
+      text = msgResponse.msg2;
+  }
+  else {
+      text = msgResponse.msg3;
+  }
+  console.log(text);
+  msgContainer.innerText = text;     
   
 }
 
-function createListElement(text) {
-  const liElement = document.createElement('li');
-  liElement.innerText = text;
-  return liElement;
+function createMap() {
+  const map = new google.maps.Map(
+      document.getElementById('map'),
+      {center: {lat: 37.422, lng: -122.084}, zoom: 16});
+}
+
+function load(){
+    showMessage();
+    createMap();
 }
